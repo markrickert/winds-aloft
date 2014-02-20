ec3f56c62101a6fc18678b9263fc30bd388ce836
Mark Rickert <mjar81@gmail.com>
2014-02-20 18:15:47 -0500

    Update home route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 WindsAloft::Application.routes.draw do
 
-  # You can have the root of your site routed with "root"
-  # root 'winds#index'
+  root to: 'winds#index'
 
   resources :stations, only: [:index]
   resources :winds, only: [:index]
